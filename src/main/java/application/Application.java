edaed6b26e6aa85353766cfc07e9bd9ce6c42d20
NickBeauson <NickBeauson-r0255890@github.com>
2018-12-03 20:25:52 +0100

    frontend

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -50,6 +52,14 @@ public class Application {
             orderRepository.save(order);
 
         };
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry){
+            registry.addMapping("/**");
+        }
     }
 
 
